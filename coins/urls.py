d3b20b9cdc06cfc2df09abe3659f6b3395ebea3d
Aleksey <web.ams@yandex.ru>
2023-11-16 15:58:22 +0300

    Закончил создание страниц

--- coins/urls.py
+++ coins/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CoinsViews.as_view())
+    path('', views.CoinsViews.as_view()),
+    path('<slug:slug>/', views.CoinDetail.as_view())
 ]
--- coins/views.py
+++ coins/views.py
@@ -7,3 +7,9 @@ class CoinsViews(View):
     def get(self, request):
         coins = Coin.objects.all()
         return render(request, 'coins/coin.html', {'coin_list': coins})
+
+class CoinDetail(View):
+    '''Представление одной монеты'''
+    def get(self, request, slug):
+        coin = Coin.objects.get(url=slug)
+        return render(request, 'coins/coin_detail.html', {'coin': coin})
